29a3dc7a11f85a56dc58fe0cb90e16c07de72cfd
GaneshLathin <selvaraj06061974@gmail.com>
2026-02-17 10:31:53 +0530

    Sort Characters By Frequency

---
+++ DSA in C++/STRING/Sort-Characters-By-Frequency.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        
+        // Step 1: Count frequency
+        unordered_map<char,int> um;
+        for(auto x : s) {
+            um[x]++;
+        }
+
+        // Step 2: Max heap (frequency, character)
+        priority_queue<pair<int,char>> pq;
+        for(auto it : um) {
+            pq.push({it.second, it.first});
+        }
+
+        // Step 3: Build answer
+        string ans = "";
+        while(!pq.empty()) {
+            auto top = pq.top();
+            pq.pop();
+            
+            ans += string(top.first, top.second);
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    string s;
+    
+    cout << "Enter string: ";
+    cin >> s;
+
+    Solution obj;
+    string result = obj.frequencySort(s);
+
+    cout << "Sorted by frequency: " << result << endl;
+
+    return 0;
+}
