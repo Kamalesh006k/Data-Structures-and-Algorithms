6c4c8fe5d8d78a6f0488201c5d6eb2c02f600dc9
GaneshLathin <selvaraj06061974@gmail.com>
2026-02-26 09:32:25 +0530

    numSteps

---
+++ DSA in C++/BITS/numSteps.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int numSteps(string s) {
+        int c = 0;
+        
+        while (s.size() > 1) {
+            // If even → divide by 2
+            if (s.back() == '0') {
+                s.pop_back();
+                c++;
+                continue;
+            }
+            
+            // If odd → add 1
+            bool check = true;
+            
+            for (int i = s.size() - 1; i >= 0; i--) {
+                if (s[i] == '0') {
+                    s[i] = '1';
+                    check = false;
+                    break;
+                }
+                s[i] = '0';
+            }
+            
+            // If all bits were 1 (like 111 → 1000)
+            if (check)
+                s = '1' + s;
+            
+            c++;
+        }
+        
+        return c;
+    }
+};
+
+int main() {
+    string s;
+    
+    cout << "Enter binary string: ";
+    cin >> s;
+
+    Solution obj;
+    int result = obj.numSteps(s);
+
+    cout << "Number of steps: " << result << endl;
+
+    return 0;
+}
