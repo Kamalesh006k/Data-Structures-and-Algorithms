837dff48fc4aa728bd3649899b1fee585fcab449
GaneshLathin <selvaraj06061974@gmail.com>
2026-03-06 21:13:55 +0530

    checkOnesSegment

---
+++ DSA in C++/BITS/checkOnesSegment.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool checkOnesSegment(string s) {
+        for(int i = 0; i < s.size() - 1; i++) {
+            if(s[i] == '0' && s[i+1] == '1') {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    string s;
+    cin >> s;
+
+    Solution obj;
+    
+    if(obj.checkOnesSegment(s))
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;
+}
