211dc221bb4480c33efbcc14e4eb526f0b860005
GaneshLathin <selvaraj06061974@gmail.com>
2026-03-09 19:37:27 +0530

    Greedy folder added with maximum possible value problem

---
+++ DSA in C++/ARRAY/maximum-possible-value.cpp
@@ -0,0 +1,35 @@
+// You are using GCC
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    int sum=0,p=0,ne=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+        sum+=abs(v[i]);
+        if(v[i]<0 )
+        {
+            ne++;
+        }
+        else
+        p++;
+    }
+    int small=INT_MAX;
+    for(auto x : v)
+    {
+        if(abs(x)<small)
+        small=abs(x);
+    }
+    if((p==0 && ne>0) || (p>0 && ne==0))
+    {
+        cout<<sum-(2*small);
+    }
+    else
+    {
+        cout<<sum;
+    }
+}
