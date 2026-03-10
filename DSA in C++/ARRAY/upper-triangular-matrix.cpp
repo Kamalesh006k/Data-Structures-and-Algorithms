8713fd1274b996d1e4ab8c11de65e1809952491d
GaneshLathin <selvaraj06061974@gmail.com>
2026-03-10 18:42:25 +0530

    upper triangular matrix

---
+++ DSA in C++/ARRAY/upper-triangular-matrix.cpp
@@ -0,0 +1,26 @@
+// You are using GCC
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>>v(n,vector<int>(m));
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>v[i][j];
+        }
+    }
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(i<=j)
+            sum+=v[i][j];
+        }
+    }
+    cout<<sum;
+}
