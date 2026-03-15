0bb583e0a7c84aea924c141e896b6ea24baa98e8
GaneshLathin <selvaraj06061974@gmail.com>
2026-03-15 22:56:56 +0530

    updated Maximum sum path

--- DSA in C++/TREES/BINARY-TREE/Maximum-sum-path.cpp
+++ DSA in C++/TREES/BINARY-TREE/Maximum-sum-path.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
+// Maximum Path Sum in Binary Tree  
+// A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
 struct node
 {
     int d;
