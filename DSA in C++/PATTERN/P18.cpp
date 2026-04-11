eef7e907a69f474ff3512000800201aaafdafd98
GaneshLathin <selvaraj06061974@gmail.com>
2026-04-11 19:37:20 +0530

    Patterns problem 18

---
+++ DSA in C++/PATTERN/P18.cpp
@@ -0,0 +1,21 @@
+// You are using GCC
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+   
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i==0 || j==n-1 ||i==n-1 ||j==0)
+            cout<<"*";
+            else 
+            cout<<" ";
+            
+        }
+        cout<<endl;
+    }
+}
