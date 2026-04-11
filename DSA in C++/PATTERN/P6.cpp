4d1678768af5636300ce536082ae8b5fd5671d92
GaneshLathin <selvaraj06061974@gmail.com>
2026-04-11 15:43:28 +0530

    Patterns problem 6

---
+++ DSA in C++/PATTERN/P6.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int k=1;
+        for(int j=n-1;j>=i;j--)
+        {
+            cout<<k++<<" ";
+        }
+        cout<<endl;
+    }
+}
