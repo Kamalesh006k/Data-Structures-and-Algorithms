57a3f0ab8948bd309c55ef77bcb660a36ad73a57
GaneshLathin <selvaraj06061974@gmail.com>
2026-04-11 16:39:58 +0530

    Patterns problem 10

---
+++ DSA in C++/PATTERN/P10.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int s=0;s<n-i-1;s++)
+        {
+            cout<<"  ";
+        }
+        for(int j=0;j<2*i+1;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int i=1;i<n;i++)
+    {
+        for(int j=0;j<2*n-i-1;j++)
+        {
+            if(j<i)
+            cout<<"  ";
+            else
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
