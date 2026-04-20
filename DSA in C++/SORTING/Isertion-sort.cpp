8c41af99b5fcbfe613fb7dbfdedacf68489a416c
GaneshLathin <selvaraj06061974@gmail.com>
2026-04-20 20:26:18 +0530

    Insertion  sort

---
+++ DSA in C++/SORTING/Isertion-sort.cpp
@@ -0,0 +1,30 @@
+// You are using GCC
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    
+    for(int i=1;i<n;i++)
+    {
+        int temp=v[i];
+        int j=i-1;
+        while(j>=0 && v[j] >temp)
+        {
+            v[j+1]=v[j];
+            j--;
+        }
+        v[j+1]=temp;
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+        cout<<v[i]<<" ";
+    }
+}
