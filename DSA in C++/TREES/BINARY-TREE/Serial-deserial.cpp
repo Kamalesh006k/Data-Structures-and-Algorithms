13f4ecdbc87114768635f9f01c27304e7af5e999
GaneshLathin <selvaraj06061974@gmail.com>
2026-06-13 21:31:17 +0530

    serialize and deserialize

--- DSA in C++/TREES/BINARY-TREE/Serial-deserial.cpp
+++ DSA in C++/TREES/BINARY-TREE/Serial-deserial.cpp
@@ -1,71 +1,163 @@
-// You are using GCC
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-// Serialize and Deserialize Binary Tree
-struct node
-{
-    string d;
-    node *l,*r;
-    node(string d)
-    {
-        this->d=d;
-        this->l=NULL;
-        this->r=NULL;
+
+/* Tree Node */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) {
+        val = x;
+        left = right = NULL;
     }
 };
 
-node* insert(vector<string>&v,int n,int &i)
-{
-    if(i>=n || v[i]=="#")
-    {
-        i++;
-        return NULL;
+/* Codec Class */
+class Codec {
+public:
+
+    string serialize(TreeNode* root) {
+        if(root==NULL) return "";
+
+        string res="";
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty())
+        {
+            TreeNode* x = q.front();
+            q.pop();
+
+            if(x==NULL)
+            {
+                res += "#,";
+            }
+            else
+            {
+                res += to_string(x->val) + ",";
+                q.push(x->left);
+                q.push(x->right);
+            }
+        }
+        return res;
     }
 
-    node* temp=new node(v[i++]);
-    temp->l=insert(v,n,i);
-    temp->r=insert(v,n,i);
+    TreeNode* deserialize(string data) {
+        if(data.size()==0) return NULL;
 
-    return temp;
-}
+        stringstream ss(data);
+        string s;
 
-void preorder(node* root,vector<string>&res)
-{
-    if(root==NULL)
-    {
-        res.push_back("#");
-        return;
+        getline(ss,s,',');
+        TreeNode* root = new TreeNode(stoi(s));
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty())
+        {
+            TreeNode* x = q.front();
+            q.pop();
+
+            // LEFT
+            if(!getline(ss,s,',')) break;
+            if(s != "#")
+            {
+                x->left = new TreeNode(stoi(s));
+                q.push(x->left);
+            }
+
+            // RIGHT
+            if(!getline(ss,s,',')) break;
+            if(s != "#")
+            {
+                x->right = new TreeNode(stoi(s));
+                q.push(x->right);
+            }
+        }
+        return root;
     }
+};
 
-    res.push_back(root->d);
-    preorder(root->l,res);
-    preorder(root->r,res);
-}
+/* Build Tree from Level Order Input */
+TreeNode* buildTree(string input) {
+    if(input.size() == 0) return NULL;
 
-int main()
-{
-    vector<string>v;
+    stringstream ss(input);
     string s;
 
-    while(cin>>s)
-        v.push_back(s);
+    getline(ss, s, ',');
+    if(s == "#") return NULL;
 
-    // Print input directly as Serialized tree
-    cout<<"Serialized tree: ";
-    for(auto x : v)
-        cout<<x<<" ";
+    TreeNode* root = new TreeNode(stoi(s));
+    queue<TreeNode*> q;
+    q.push(root);
 
-    // Build tree (Deserialize)
-    int i=0;
-    node* root=insert(v,v.size(),i);
+    while(!q.empty()) {
+        TreeNode* curr = q.front();
+        q.pop();
 
-    // Serialize again
-    vector<string>res;
-    preorder(root,res);
+        // LEFT
+        if(!getline(ss, s, ',')) break;
+        if(s != "#") {
+            curr->left = new TreeNode(stoi(s));
+            q.push(curr->left);
+        }
 
-    cout<<"Deserialized tree: ";
-    for(auto x : res)
-        cout<<x<<" ";
+        // RIGHT
+        if(!getline(ss, s, ',')) break;
+        if(s != "#") {
+            curr->right = new TreeNode(stoi(s));
+            q.push(curr->right);
+        }
+    }
+    return root;
+}
+
+/* Print Level Order */
+void printLevel(TreeNode* root) {
+    if(!root) return;
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while(!q.empty()) {
+        TreeNode* x = q.front();
+        q.pop();
+
+        if(x == NULL) {
+            cout << "# ";
+            continue;
+        }
+
+        cout << x->val << " ";
+        q.push(x->left);
+        q.push(x->right);
+    }
+}
+
+/* MAIN */
+int main() {
+    Codec ser, deser;
+
+    cout << "Enter tree in level order (use # for NULL, comma separated):\n";
+
+    string input;
+    cin >> input;
+
+    // Build tree
+    TreeNode* root = buildTree(input);
+
+    // Serialize
+    string serialized = ser.serialize(root);
+    cout << "\nSerialized: " << serialized << endl;
+
+    // Deserialize
+    TreeNode* newRoot = deser.deserialize(serialized);
+
+    // Print tree
+    cout << "\nTree after Deserialization (level order): ";
+    printLevel(newRoot);
 
     return 0;
 }
